a70c64036da51091226a910e64bab2aa1100e4cf
Teo <tsmastro@uwaterloo.ca>
2016-08-13 13:04:24 -0400

    SearchBox

--- studyapp/app/controllers/landing.js
+++ studyapp/app/controllers/landing.js
@@ -1,5 +1,34 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	
+	uni: null,
+	uniError: false,
+	universities: 
+	[
+		{
+			name: 'University of Waterloo',
+			shortHand: 'UW'
+		},
+		{
+			name: 'University of Toronto',
+			shortHand: 'UofT'
+		},
+		{
+			name: 'Wilfred Laurier University',
+			shortHand: 'WLU'
+		}
+	],
+
+	actions: {
+		chooseUniversity(uni) {
+			if (this.get('uniError'))
+				this.set('uniError', false);
+			this.set('uni', uni);
+		},
+		submitUni: function() {
+			var uniName = this.get('uni').shortHand;
+			this.transitionTo('sign-up', { queryParams: {uni: uniName}});
+  	}
+	}
 });
+
--- studyapp/app/controllers/sign-up.js
+++ studyapp/app/controllers/sign-up.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	queryParams: ['uni'],
 	alreadySignedUp: false,
 	uni: null,
 	selectedCourses: [],
